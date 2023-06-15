638785d9d5831a6d1e89793707f2274bbc77f9c6
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-06-15 15:45:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with prinf function
  *
  * Return: Always 0 (Success)
  */
@@ -11,5 +11,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }
