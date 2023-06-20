52887ab772d68a64e15057d946ec4593b83f93d5
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-06-20 15:22:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char su[] = "_putchar";
+	int x;
+
+	for (x = 0; x <= 7; x++)
+	{
+		putchar(su[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
