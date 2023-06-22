7bfdf820f999dd16e3ac9dcf5df374ac4f45488a
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-06-22 13:17:53 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul ->
+ * is a function that myltiplies two integers.
+ *
+ * @a: a is an integer parameter
+ * @b: b is an integer parameters
+ * Return: int.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
