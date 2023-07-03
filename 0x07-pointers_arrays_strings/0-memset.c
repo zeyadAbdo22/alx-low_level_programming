ad46afb3dc170448e09099937744c56546e3c7cc
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-07-03 15:18:45 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
         unsigned int x;
 
-        for (i = 0; n > 0 ; i++, n--)
+        for (x = 0; n > 0 ; x++, n--)
         {
                 s[x] = b;
         }
